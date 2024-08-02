ba117dd74c9fa0f0085473ba89c62c0131624424
Pinak Nayak <77262741+npinak@users.noreply.github.com>
2024-08-02 12:41:08 -0700

    Feat:basic styles done

--- fingerpaint-front/src/app/page.tsx
+++ fingerpaint-front/src/app/page.tsx
@@ -2,13 +2,43 @@ import { AppBar, Box, Container, Toolbar } from '@mui/material'
 
 export default function Home() {
   return (
-    <main id='main-page' style={{ height: '100%' }}>
+    <main id='main-page' style={{ height: '100%', padding: '8px' }}>
       <AppBar position='sticky'>
         <Toolbar sx={{ height: '8%' }}></Toolbar>
       </AppBar>
-      <Container sx={{}} id='main-section'>
-        <Box>
-          <p>Main Section</p>
+      <Container
+        sx={{
+          margin: 0,
+          padding: '0px',
+          backgroundColor: '#e5e5e5',
+          display: 'flex',
+          height: '92%',
+          justifyContent: 'space-around',
+        }}
+        maxWidth='xl'
+        // disableGutters
+        id='main-section'
+      >
+        <Box
+          sx={{
+            height: '30%',
+            width: '8%',
+            backgroundColor: '#f4f1de',
+            marginTop: '40px',
+          }}
+        >
+          {' '}
+          ToolBar
+        </Box>
+        <Box
+          sx={{
+            height: '90%',
+            width: '75%',
+            backgroundColor: '#f4f1de',
+            marginTop: '40px',
+          }}
+        >
+          <p>Drawing Board</p>
         </Box>
       </Container>
     </main>
