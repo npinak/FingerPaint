a64f83935888e896f4682749c558d1459cb1557b
Pinak Nayak <77262741+npinak@users.noreply.github.com>
2024-08-01 21:29:54 -0700

    basic-layout

--- fingerpaint-front/src/app/page.tsx
+++ fingerpaint-front/src/app/page.tsx
@@ -1,6 +1,16 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import { AppBar, Box, Container, Toolbar } from '@mui/material'
 
 export default function Home() {
-  return <main id='main-page'>Main Page</main>
+  return (
+    <main id='main-page' style={{ height: '100%' }}>
+      <AppBar position='sticky'>
+        <Toolbar sx={{ height: '8%' }}></Toolbar>
+      </AppBar>
+      <Container sx={{}} id='main-section'>
+        <Box>
+          <p>Main Section</p>
+        </Box>
+      </Container>
+    </main>
+  )
 }
